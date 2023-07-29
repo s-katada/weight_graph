205d3fd087925cc1027bb0ffe02afca34cd0ad2d
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2023-07-29 13:07:00 +0900

    feat: 日付のフォーマットを修正

--- api/app/controllers/weight_logs_controller.rb
+++ api/app/controllers/weight_logs_controller.rb
@@ -2,7 +2,7 @@
 class WeightLogsController < ApplicationController
   def index
     weight_logs = WeightLog.all
-    proceeded_weight_logs = weight_logs.map { |wl| { weight: wl.weight, date: l(wl.date, format: :default) } }
+    proceeded_weight_logs = weight_logs.map { |wl| { weight: wl.weight, date: I18n.l(wl.date, format: :short) } }
     render json: proceeded_weight_logs
   end
 
