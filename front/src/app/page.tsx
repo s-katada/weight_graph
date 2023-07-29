62fd3f50c9f52bfce170a057664fec470335fa5e
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2023-07-29 13:17:43 +0900

    feat: weight_logのデータを取得する処理を追加

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,22 +2,32 @@
 
 import { Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
 
+interface WeightLog {
+  weight: number;
+  date: string;
+}
+
 async function getWeightLog() {
-  return axios.get('http://localhost:3000/weight_logs')
-              .then((response) => {
-                console.log(response);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
+  return axios
+    .get('http://localhost:3000/weight_logs')
+    .then((response: AxiosResponse<WeightLog[]>) => {
+      return response.data;
+    })
+    .catch((_error) => {
+      return [] as WeightLog[];
+    });
 }
 
 export default function Home() {
+  const [weightLog, setWeightLog] = useState<WeightLog[]>([]);
+  console.log(weightLog);
   useEffect(() => {
-    getWeightLog();
+    (async () => {
+      setWeightLog(await getWeightLog());
+    })();
   }, []);
   return (
     <Layout>
